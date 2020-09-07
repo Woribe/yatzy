c71db90492b7faa4f4f5675903939b1280e34d12
rune <rune.krogh.leevre@gmail.com>
2020-09-07 12:45:15 +0200

    js template

---
+++ function.js
@@ -0,0 +1,28 @@
+let dice = document.querySelectorAll(dice);
+let throws = 0;
+
+function throws() {
+  return -1;
+}
+
+function resetThrow() {
+  return -1;
+}
+
+// Beregninger
+//frequency regner hvor mange af hver der er...
+function frequency() {}
+
+function sameValuePoints(value) {}
+
+function onePair() {}
+function twoPairs() {}
+function threeSame() {}
+
+function fourSame(params) {}
+function fullHouse(params) {}
+function small(params) {}
+function large(params) {}
+function chance(params) {}
+function yatzy(params) {}
+function total(params) {}
